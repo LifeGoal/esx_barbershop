9bfb564869f5ddbcaf129e4625c8b6d16867989a
LifeGoal <35453587+LifeGoal@users.noreply.github.com>
2018-07-23 17:11:45 +0200

    Added sv.lua to locales

---
+++ locales/sv.lua
@@ -0,0 +1,10 @@
+Locales['sv'] = {
+
+    ['valid_purchase'] = 'godkänner du köpet?',
+    ['yes'] = 'ja',
+    ['no'] = 'nej',
+    ['not_enough_money'] = 'du har inte tillräckligt med pengar',
+    ['press_access'] = 'klicka på ~INPUT_CONTEXT~ för att öppna menyn',
+    ['barber_blip'] = 'frisörsalong',
+    ['you_paid'] = 'du betalade ',
+}
